d77e319b2fc88aad321eab82db099bfbf8faae63
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-01-09 23:46:25 -0500

    feat: cree una plantilla con el menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'Inventario SmartInfo' }}</title>
+    <title>Inventario SmartInfo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">{{ $title ?? 'Inventario SmartInfo' }}</a>
+            <a class="navbar-brand" href="{{ route('principal') }}">Inventario SmartInfo</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -30,10 +30,10 @@
                         </li>
                     @else
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Login</a>
+                            <a class="nav-link" href="{{route('Login')}}">Login</a>
                         </li>
                     @endauth
-                    <li class="nav-item"><a class="nav-link active" href="{{ url('/') }}">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="{{ route('principal') }}">Inicio</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Productos</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Categorías</a></li>
                     <li class="nav-item"><a class="nav-link" href="#">Movimientos</a></li>
