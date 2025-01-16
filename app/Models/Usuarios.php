7fe85a1101eab9f1ba10573f360d2efa0336466d
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-01-16 09:35:59 -0500

    feat : modifique el modelo de los usuarios

--- app/Models/Usuarios.php
+++ app/Models/Usuarios.php
@@ -1,26 +1,71 @@
 <?php
 
+// namespace App\Models;
+
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+
+// class Usuarios extends Model
+// {
+//     use HasFactory;
+
+//     protected $tabla = 'usuarios';
+    
+//     protected $fillable = [
+//         'dni',
+//         'rol',
+//         'nombre',
+//         'apellido',
+//         'telefono',
+//         'direccion',
+//         'correo',
+//         'contraseña',
+//     ];
+
+// }
+
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Usuarios extends Model
+class Usuarios extends Authenticatable
 {
     use HasFactory;
 
-    protected $tabla = 'usuarios';
-    
+    // Especificar la tabla 'usuarios' en lugar de 'users'
+    protected $table = 'usuarios';
+
     protected $fillable = [
-        'dni',
-        'rol',
-        'nombre',
-        'apellido',
-        'telefono',
-        'direccion',
-        'correo',
+        'dni', 'nombre', 'apellido', 'telefono', 'direccion', 'correo', 'contraseña', 'fecha_ingreso', 'rol_id'
+    ];
+
+    protected $hidden = [
         'contraseña',
     ];
 
+    //
+public function updateUser($data)
+{
+    $this->dni = $data['dni'];
+    $this->nombre = $data['nombre'];
+    $this->apellido = $data['apellido'];
+    $this->telefono = $data['telefono'];
+    $this->direccion = $data['direccion'];
+    $this->correo = $data['correo'];
+    if (isset($data['password'])) {
+        $this->password = bcrypt($data['password']);
+    }
+    $this->fecha_ingreso = $data['fecha_ingreso'];
+    $this->rol_id = $data['rol_id'];
+    $this->save();
 }
+
+public function deleteUser()
+{
+    $this->delete();
+}
+}
+
+
